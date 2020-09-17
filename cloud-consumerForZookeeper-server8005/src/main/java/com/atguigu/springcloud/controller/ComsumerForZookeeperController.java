abeeb94da656b83101d646fb55a9cc9f26ed537e
liuss <liuss@sh-stt.com>
2020-09-17 16:59:46 +0800

    、修改

--- cloud-consumerForZookeeper-server8005/src/main/java/com/atguigu/springcloud/controller/ComsumerForZookeeperController.java
+++ cloud-consumerForZookeeper-server8005/src/main/java/com/atguigu/springcloud/controller/ComsumerForZookeeperController.java
@@ -16,7 +16,8 @@ public class ComsumerForZookeeperController {
     @Autowired
     private RestTemplate restTemplate;
 
-    private String  URL_Prifix = "http://127.0.0.1:8004";
+   // private String  URL_Prifix = "http://127.0.0.1:8004"; 写死的http地址不利于扩展
+    private String  URL_Prifix = "http://zookeeper-server";  //zookeeper注册中心上注册的微服务名称这样利于扩展(此服务名可以通过在zookeeper的Client端查找通过 ls / 一层一层查找，，get /xxx 是查看zookeeper上注册的节点具体信息) 注册到zookeeper的节点均属于临时节点
 
     @RequestMapping("/getConsumer/for/zookeeper")
     public String getMsgForZookeeper(){
