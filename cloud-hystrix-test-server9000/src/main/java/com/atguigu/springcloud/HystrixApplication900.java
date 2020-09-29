fda59e78e9f34cef3ae1b3e78a1db2f1ac4984d3
liuss <liuss@sh-stt.com>
2020-09-29 13:03:12 +0800

    监控平台dashboard的测试案例

--- cloud-hystrix-test-server9000/src/main/java/com/atguigu/springcloud/HystrixApplication900.java
+++ cloud-hystrix-test-server9000/src/main/java/com/atguigu/springcloud/HystrixApplication900.java
@@ -1,9 +1,12 @@
 package com.atguigu.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * description: HystrixApplication900 <br>
@@ -20,4 +23,18 @@ public class HystrixApplication900 {
     public static void main(String[] args) {
         SpringApplication.run(HystrixApplication900.class,args);
     }
+
+
+    /*   此配置是为了服务监控而配置的,与服务容错本身无关,springcloud升级后的bug
+    servletRegistrationBean因为springboot的默认路径不是"/hystrix.stream",
+    只要在自己的项目中配置上下面的servlet的就可以。*/
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);    registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");    return registrationBean;
+    }
+    //注意；使用dashboard监控平台的时候需要引入spring-boot-starter-actuator这个依赖jar包，并且在主启动类开启了@EnableCircuitBreaker注解方可进行监控
+    //cloud-hystrix-test-dashboard是监控平台，例如他监控9000这个服务，因此在启动服务时候输入localhost:9001(端口)/hystrix  ----> 在界面地址中输入监控的对象localhost:9000/hystrix.stream即可看到图形化界面
 }
--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMainApplication.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMainApplication.java
@@ -1,10 +1,13 @@
 package com.atguigu.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * springboot启动类微服务的入口
@@ -12,7 +15,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication                                    //springBoot的主启动注解是一个复合注解包括（SpringBootConfiguration , EnableAutoConfiguration,ComponentScan）三个主要注解
 @MapperScan("com.atguigu.springcloud.mapper.*")           //扫描Mapper层接口的类
 @EnableDiscoveryClient   //开启注册中心客户端的服务发现（注解:可以支持eureka、zookeeper、nocas注册中心） @EnableEurekaClient 此注解只支持eureka的注册中心 项目中通常使用@EnableDiscoveryClient
-@EnableCircuitBreaker() //SpringCloud中使用断路器，需要加上此注解
+@EnableCircuitBreaker() //开启SpringCloud中使用断路器，需要加上此注解
 public class PaymentMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMainApplication.class,args);
