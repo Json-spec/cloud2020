0cb8bfabe393ef739d63ab42d0d22cb97f56487d
liuss <liuss@sh-stt.com>
2020-10-20 18:24:01 +0800

    添加开启swagger2的功能注解

--- cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/alibaba/PaymentApplication9001.java
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/alibaba/PaymentApplication9001.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud.alibaba;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * description: PaymentApplication9001 <br>
@@ -12,6 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableSwagger2  //开启swagger2的功能注解
 public class PaymentApplication9001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication9001.class,args);
